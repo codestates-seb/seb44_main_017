120e0fedaf9618f094f9ef40ec8e12b9803918a8
yelm <21yrshin@naver.com>
2023-07-02 18:05:50 +0900

    [feat] Product Entity, Service, Dto, mapper, repo added. Read Operation OK. ProductComment entity added

--- server/src/main/java/com/main/project/exception/ExceptionCode.java
+++ server/src/main/java/com/main/project/exception/ExceptionCode.java
@@ -6,7 +6,8 @@ import lombok.Getter;
 public enum ExceptionCode {
     MEMBER_NOT_FOUND(404, "Member not found"),
     MEMBER_EXISTS(409, "Member exists"),
-    MEMBER_EMAIL_EXISTS(409, "Email exists");
+    MEMBER_EMAIL_EXISTS(409, "Email exists"),
+    PRODUCT_NOT_FOUND(404, "Product not found");
     private final int status;
     private final String message;
 
