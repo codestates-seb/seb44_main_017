0812c94943c79e5dc4cb911eb4e2fd9426d75f82
g4dalcom <g4dalcom@gmail.com>
2023-07-03 09:24:53 +0900

    [init] 초기 세팅

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>초기 세팅</div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
