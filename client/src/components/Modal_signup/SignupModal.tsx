bea22deeb0aa4d55e2ef674156676f84a7f5b05e
lhyjjg <leehyun147@gmail.com>
2023-07-04 12:45:27 +0900

    feat: signup component

--- client/src/components/Modal_signup/SignupModal.tsx
+++ client/src/components/Modal_signup/SignupModal.tsx
@@ -1,15 +1,164 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import * as S from "./style";
+import { Logo } from "./Logo";
 
 const SignupModal = () => {
+  // 인풋 값
+  const [email, setEmail] = useState<string>("");
+  const [name, setName] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [checkPassword, setCheckPassword] = useState<string>("");
+  const [phone, setPhone] = useState<string>("");
+  // 유효성 검사
+  const [emailError, setEmailError] = useState<boolean>(false);
+  const [nameError, setNameError] = useState<boolean>(false);
+  const [passwordError, setPasswordError] = useState<boolean>(false);
+  const [checkPasswordError, setCheckPasswordError] = useState<boolean>(false);
+  const [phoneError, setPhoneError] = useState<boolean>(false);
+  // 비밀번호 표시
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const [showCheckPassword, setCheckShowPassword] = useState<boolean>(false);
+  // 관리자 여부
+  const [isAdmin, setIsAdmin] = useState<boolean>(false);
+
+  const navigate = useNavigate();
+
+  // 비밀번호 확인 아이콘
+  const passwordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+  const checkPasswordVisibility = () => {
+    setCheckShowPassword(!showCheckPassword);
+  };
+
+  const handleSignup = () => {
+    const passwordReg = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,40}$/;
+    const phoneReg = /^010-\d{4}-\d{4}$/;
+
+    !email.includes("@") ? setEmailError(true) : setEmailError(false);
+    name === "" ? setNameError(true) : setNameError(false);
+    !passwordReg.test(password)
+      ? setPasswordError(true)
+      : setPasswordError(false);
+    password !== checkPassword
+      ? setCheckPasswordError(true)
+      : setCheckPasswordError(false);
+    !phoneReg.test(phone) ? setPhoneError(true) : setPhoneError(false);
+    console.log(isAdmin);
+    if (!emailError && !nameError && !passwordError && !phoneError && isAdmin) {
+      const data = { email, name, password, phone };
+      console.log(data);
+      axios
+        .post("url", {
+          data,
+        })
+        .then(function (response) {
+          navigate("/modal_login");
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    } else if (
+      !emailError &&
+      !nameError &&
+      !passwordError &&
+      !phoneError &&
+      !isAdmin
+    ) {
+      const data = { email, name, password, phone };
+      console.log(data);
+      axios
+        .post("url", {
+          data,
+        })
+        .then(function (response) {
+          navigate("/modal_login");
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+  };
   return (
     <S.Container>
       <S.Content>
-        <S.CloseButton>
-          <S.Close />
-        </S.CloseButton>
-
-        <h1>SignupMod</h1>
+        <S.CloseButton />
+        <S.SignupTitleContainer>
+          <Logo width="40px" height="24px" />
+          <S.SignupTitle>SIGNUP</S.SignupTitle>
+        </S.SignupTitleContainer>
+        <S.Explanation>Recloset회원가입 페이지입니다.</S.Explanation>
+        <S.Explanation style={{ marginBottom: "16px" }}>
+          계정이 없으시다면 회원을 등록해주세요.
+        </S.Explanation>
+        <S.InputBox
+          type="text"
+          value={email}
+          placeholder="이메일을 입력해주세요."
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <S.NameLabel>
+          <S.InputBox
+            type="text"
+            value={name}
+            placeholder="닉네임을 입력해주세요."
+            onChange={(e) => setName(e.target.value)}
+          />
+          <S.DuplicateCheck>중복 검사</S.DuplicateCheck>
+        </S.NameLabel>
+        <S.PasswordLabel>
+          <S.InputBox
+            type={showPassword ? "text" : "password"}
+            value={password}
+            placeholder="비밀번호를 입력해주세요."
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <S.VisibilityButton onClick={passwordVisibility} />
+        </S.PasswordLabel>
+        <S.PasswordLabel>
+          <S.InputBox
+            type={showCheckPassword ? "text" : "password"}
+            value={checkPassword}
+            placeholder="비밀번호를 확인합니다."
+            onChange={(e) => setCheckPassword(e.target.value)}
+          />
+          <S.VisibilityButton onClick={checkPasswordVisibility} />
+        </S.PasswordLabel>
+        <S.InputBox
+          style={{ marginBottom: "4px" }}
+          placeholder="핸드폰 번호를 입력해주세요."
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+        />
+        {emailError && (
+          <S.ErrorMsg>이메일은 '@'이 포함되어야합니다.</S.ErrorMsg>
+        )}
+        {nameError && <S.ErrorMsg>닉네임을 입력해주세요.</S.ErrorMsg>}
+        {passwordError && (
+          <S.ErrorMsg>
+            비밀번호는 문자, 숫자 포함 8~40자로 입력해주세요.
+          </S.ErrorMsg>
+        )}
+        {checkPasswordError && (
+          <S.ErrorMsg>비밀번호를 다시 확인해주세요.</S.ErrorMsg>
+        )}
+        {phoneError && (
+          <S.ErrorMsg>
+            핸드폰 번호는 11자리 숫자와 '-'로 구성되어야 합니다.
+          </S.ErrorMsg>
+        )}
+        <S.AdminLabel>
+          <input
+            type="checkbox"
+            checked={isAdmin}
+            onChange={(e) => setIsAdmin(e.target.checked)}
+            style={{ cursor: "pointer" }}
+          />
+          관리자로 회원가입하기
+        </S.AdminLabel>
+        <S.SignupButton onClick={handleSignup}>SIGNUP</S.SignupButton>
       </S.Content>
     </S.Container>
   );
