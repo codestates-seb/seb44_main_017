46939547e4ef5655414fcbab6a1ef1ae1a1f66af
오승찬 <oseungchan@oseungchan-ui-MacBookAir.local>
2023-07-05 13:46:03 +0900

    [feat]Header

---
+++ client/src/components/Header/styled.tsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const LoginBtn = styled.button`
+  border-radius: 16px;
+  width: 96px;
+  height: 32px;
+  font-weight: var(--font-weight-700);
+  margin-right: 16px;
+  font-size: var(--font-size-12);
+  border: 0;
+  background-color: var(--color-darkblue);
+  color: var(--color-white);
+`;
+export const SignupBtn = styled.button`
+  border-radius: 16px;
+  width: 96px;
+  height: 32px;
+  font-weight: var(--font-weight-700);
+  font-size: var(--font-size-12);
+  border: 2px solid var(--color-darkblue);
+  background-color: var(--color-white);
+  color: var(--color-darkblue);
+`;
+export const NavBtn = styled.a`
+  color: black;
+  font-weight: var(--font-weight-700);
+  font-size: var(--font-size-16);
+  margin-right: 36px;
+  cursor: pointer;
+`;
