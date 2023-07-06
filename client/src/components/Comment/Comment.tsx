292ad3511bbaa2b9a88562aba5536b422f4dc6c6
g4dalcom <g4dalcom@gmail.com>
2023-07-06 18:00:32 +0900

    [feat] comment mok데이터 이용해서 기초 기능 구현

--- client/src/components/Comment/Comment.tsx
+++ client/src/components/Comment/Comment.tsx
@@ -1,9 +1,81 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import useInput from "../../hooks/useInput";
+import axios from "axios";
 
-const Comment = () => {
-  return (
-    <div>Comment</div>
-  )
+interface CommentProps {
+  commentId: string;
+  content: string;
+  createAt: string;
+  modifyAt: string;
+  writer: {
+    memberId: string;
+    name: string;
+  };
 }
 
-export default Comment
+const Comment = () => {
+  const initialValue: CommentProps = {
+    commentId: "",
+    content: "",
+    createAt: "",
+    modifyAt: "",
+    writer: {
+      memberId: "",
+      name: "",
+    },
+  };
+
+  const [commentValue, changeHandler, reset] = useInput("");
+  const [commentList, setCommentList] = useState<CommentProps[]>([
+    initialValue,
+  ]);
+  console.log("commentValue = ", commentValue);
+  console.log("list = ", commentList);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5173/src/moks/comment.json")
+      .then(res => setCommentList(res.data));
+  }, []);
+
+  const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("commentValue = ", commentValue);
+    reset();
+  };
+
+  return (
+    <div>
+      <form onSubmit={submitHandler}>
+        <input
+          type="text"
+          onChange={changeHandler}
+          value={commentValue}
+          placeholder="Add Comment..."
+        />
+        <button>작성</button>
+      </form>
+      <div>
+        {commentList.length > 0 &&
+          commentList.map(e => (
+            <>
+              <div key={e.commentId}>
+                <div>
+                  <span>{e.writer.name}</span>
+                  <span>{e.createAt}</span>
+                </div>
+                <div>{e.content}</div>
+              </div>
+              // 오른쪽
+              <div>
+                <button>수정</button>
+                <button>삭제</button>
+              </div>
+            </>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default Comment;
