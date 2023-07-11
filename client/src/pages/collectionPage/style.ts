0ca6dc1741d17694b64eeb5504f3b697a65232af
g4dalcom <g4dalcom@gmail.com>
2023-07-11 12:25:12 +0900

    [style] 의류 수거 신청 페이지 기본 골격 CSS 작업

--- client/src/pages/collectionPage/style.ts
+++ client/src/pages/collectionPage/style.ts
@@ -5,10 +5,33 @@ export const Section = styled.section`
   flex-direction: column;
   width: 100%;
   align-items: center;
+  padding-top: 54px;
 `;
 
 export const PageTitle = styled.div`
   display: flex;
   flex-direction: column;
   text-align: center;
+  padding-bottom: 40px;
+  border-bottom: 2px solid var(--color-black);
+  gap: 12px;
+  width: 67%;
+
+  & > h1 {
+    font-size: 36px;
+    font-weight: 700;
+  }
+
+  & > h4 {
+    font-size: 18px;
+    font-weight: 700;
+    color: var(--color-gray200);
+  }
+`;
+
+export const ContentsBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  margin: 36px 0;
 `;
