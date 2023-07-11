f2b49aec30368fdc72d902c369c848b03b9014da
g4dalcom <g4dalcom@gmail.com>
2023-07-11 10:43:20 +0900

    [feat] image, contents에 itemId 부여해서 하나의 객체로 통합

--- client/src/pages/collectionPage/collectionPage.tsx
+++ client/src/pages/collectionPage/collectionPage.tsx
@@ -1,5 +1,5 @@
 import CollectionForm from "../../components/Collection_form/CollectionForm";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import axios from "axios";
 
 export interface ItemProps {
@@ -11,32 +11,26 @@ export interface ItemProps {
 export interface ContentsProps {
   itemId: number;
   itemInfo: ItemProps;
+  itemImage: File | any;
 }
 
 const CollectionPage = () => {
   const initialValue = {
     itemId: 0,
     itemInfo: { name: "", content: "", category: "" },
+    itemImage: undefined,
   };
 
-  const [images, setImages] = useState<File[]>([]);
   const [contents, setContents] = useState<ContentsProps[]>([initialValue]);
   const [itemNumber, setItemNumber] = useState(1);
 
   const deleteHandler = (id: number) => {
-    console.log(id);
+    confirm("정말 삭제하시겠습니까?");
     setContents(contents.filter(item => item.itemId !== id));
-    // setImages(images.filter(item => item.itemId !== id));
   };
 
   const submitHandler = async () => {
-    console.log("images = ", images);
     console.log("contents = ", contents);
-
-    if (images.length < contents.length) {
-      alert("이미지를 등록해주세요");
-      return;
-    }
 
     for (let i = 0; i < contents.length; i++) {
       if (
@@ -46,31 +40,41 @@ const CollectionPage = () => {
       ) {
         alert("필수 항목을 모두 작성해주세요.");
         return;
+      } else {
+        if (contents[i].itemImage === null) {
+          alert("이미지를 등록해주세요.");
+          return;
+        }
       }
     }
 
     const formData = new FormData();
-    formData.append("productlist", JSON.stringify(contents));
+    const contentList = [];
 
-    for (let i = 0; i < images.length; i++) {
-      formData.append("files", images[i]);
+    for (let i = 0; i < contents.length; i++) {
+      contentList.push(contents[i].itemInfo);
+      contents[i].itemImage && formData.append("files", contents[i].itemImage);
     }
 
-    // const res = await axios.post(
-    //   "https://e087-221-148-162-66.ngrok-free.app/products/postlist",
-    //   formData,
-    //   {
-    //     headers: {
-    //       "Content-Type": "multipart/form-data",
-    //       "ngrok-skip-browser-warning": true,
-    //       Authorization:
-    //         "Bearer eyJhbGciOiJIUzM4NCJ9.eyJyb2xlcyI6WyJVU0VSIl0sInVzZXJuYW1lIjoiaGdkQGdtYWlsLmNvbSIsInN1YiI6ImhnZEBnbWFpbC5jb20iLCJpYXQiOjE2ODg5NzUxODMsImV4cCI6MTY4ODk3Njk4M30.VAz5FzGZJhTR_nGfAIB-tZgRxuNyxt0xkKpT8ipNBfd_u2MPO5tqfPa3Ivzya-V5",
-    //       Refresh:
-    //         "eyJhbGciOiJIUzM4NCJ9.eyJpc3MiOiJtZW1iZXIiLCJzdWIiOiJoZ2RAZ21haWwuY29tIiwiaWF0IjoxNjg4OTc1MTgzLCJleHAiOjE2ODkwMDAzODN9.Kn6upYn2d2gIsK1XeQaQt12gG2l4X01q-OtH5Dmkx9uRjpCOfmskxx33lyJvwTKX",
-    //     },
-    //   }
-    // );
-    // console.log(res.data);
+    formData.append("productlist", JSON.stringify(contentList));
+
+    console.log(contentList);
+
+    const res = await axios.post(
+      "https://faf9-221-148-162-66.ngrok-free.app/products/postlist",
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          "ngrok-skip-browser-warning": true,
+          Authorization:
+            "Bearer eyJhbGciOiJIUzM4NCJ9.eyJyb2xlcyI6WyJVU0VSIl0sInVzZXJuYW1lIjoiYWRtaW4xQGdtYWlsLmNvbSIsInN1YiI6ImFkbWluMUBnbWFpbC5jb20iLCJpYXQiOjE2ODkwMzY2NjMsImV4cCI6MTY4OTAzODQ2M30.cG42Ghg4RHVwqz0L1HFwcaOxBJbn_rke0NMa59Z_bc7ti--Z18WfWiJSU9_qgkDQ",
+          Refresh:
+            "eyJhbGciOiJIUzM4NCJ9.eyJpc3MiOiJtZW1iZXIiLCJzdWIiOiJhZG1pbjFAZ21haWwuY29tIiwiaWF0IjoxNjg5MDM2NjYzLCJleHAiOjE2ODkwNjE4NjN9.gWKfuHkl_sOVPX_2e0TRlyvA0pkKCxY6WJms61qiaUqYBauESiLwVYVJtJQmLEia",
+        },
+      }
+    );
+    console.log(res.data);
   };
 
   return (
@@ -89,11 +93,9 @@ const CollectionPage = () => {
             삭제
           </button>
           <CollectionForm
-            images={images}
-            setImages={setImages}
             contents={contents}
             setContents={setContents}
-            itemIndex={item.itemId}
+            itemNumber={item.itemId}
           />
         </div>
       ))}
@@ -105,6 +107,7 @@ const CollectionPage = () => {
               {
                 itemId: itemNumber,
                 itemInfo: { name: "", content: "", category: "" },
+                itemImage: undefined,
               },
             ]);
             setItemNumber(itemNumber + 1);
