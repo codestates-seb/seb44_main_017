ee8733beaf6a840c75cc8752fbc008b28aff2fbb
lhyjjg <leehyun147@gmail.com>
2023-07-18 07:57:54 +0900

    feat: 로그인 시 name, id, roles 저장

--- client/src/components/Modal_login/LoginModal.tsx
+++ client/src/components/Modal_login/LoginModal.tsx
@@ -54,14 +54,22 @@ const LoginModal = ({ closeModal }: Props) => {
 
         const response: any = await axios.post(`${BASE_URL}/user/login`, data);
         if (response.status === 200) {
-          navigate("/");
-          closeModal(false);
+          // navigate("/");
+          // closeModal(false);
           const authorization = response.headers.get("authorization");
           const refresh = response.headers.get("refresh");
-          const name = response.data;
+          const roles = response.headers.get("roles");
+          const memberId = response.headers.get("memberid");
+          const userName = response.data;
+          console.log(`헤더:${response.headers}`);
+          console.log(`바디:${response.data}`);
+          console.log(roles);
+          console.log(memberId);
           document.cookie = `authorization=${authorization}; path=/;`;
-          document.cookie = `refresh=${refresh}; path=/; SameSite=none; Secure`;
-          document.cookie = `name=${name};`;
+          document.cookie = `refresh=${refresh}; path=/;`;
+          document.cookie = `name=${userName};`;
+          document.cookie = `id=${memberId};`;
+          document.cookie = `roles=${roles};`;
         }
       } catch (error: any) {
         if (error.response.status === 404 || error.response.status === 500) {
@@ -73,17 +81,30 @@ const LoginModal = ({ closeModal }: Props) => {
         const data = { email: userName, password };
         const response: any = await axios.post(`${BASE_URL}/admin/login`, data);
         if (response.status === 200) {
-          navigate("/");
-          closeModal(false);
+          // navigate("/");
+          // closeModal(false);
           const authorization = response.headers.get("authorization");
           const refresh = response.headers.get("refresh");
-          console.log(response.data);
+          const roles = response.headers.get("roles");
+          const adminId = response.headers.get("adminid");
+          const adminName = response.data.data;
+          console.log(response);
+          console.log(`헤더:${response.headers}`);
+          console.log(adminId);
+          console.log(roles);
+          console.log(adminName);
+          // console.log(adminname);
           document.cookie = `authorization=${authorization}; path=/;`;
           document.cookie = `refresh=${refresh}; path=/; SameSite=none; Secure`;
+          document.cookie = `name=${adminName};`;
+          document.cookie = `id=${adminId};`;
+          document.cookie = `roles=${roles};`;
         }
       } catch (error: any) {
         if (error.response.status === 404 || error.response.status === 500) {
           alert("회원 정보를 확인해주세요.");
+        } else {
+          console.log(error);
         }
       }
     }
--- client/src/utils/token.tsx
+++ client/src/utils/token.tsx
@@ -12,3 +12,50 @@ export const getToken = (): Props => {
 
   return [authorization, refresh];
 };
+const [authorization, refresh] = getToken();
+console.log(authorization);
+console.log(refresh);
+
+// 사용자, 관리자 닉네임 가져오기
+export const getName = () => {
+  const cookie: string[] = document.cookie.split(";");
+  const name: string | undefined = cookie
+    .find((c) => c.includes("name="))
+    ?.replace(" name=", "");
+  return name;
+};
+console.log(getName());
+
+// 사용자, 관리자 Id 가져오기
+export const getId = () => {
+  const cookie: string[] = document.cookie.split(";");
+  const id: string | undefined = cookie
+    .find((c) => c.includes("id="))
+    ?.replace(" id=", "");
+  return id;
+};
+
+// 사용자, 관리자 구분 값 (roles)
+// 사용자인 경우 = user
+// 관리자인 경우 = admin
+export const getRoles = () => {
+  const cookie: string[] = document.cookie.split(";");
+  const roles: string | undefined = cookie
+    .find((c) => c.includes("roles="))
+    ?.replace(" roles=", "");
+  return roles;
+};
+
+// 쿠키에 저장된 정보 삭제
+// 로그아웃, 회원탈퇴 시 활용
+export const delCookie = () => {
+  const cookie: string[] = document.cookie.split(";");
+  const expiration = "Sat, 01 Jan 1972 00:00:00 GMT";
+
+  let i = 0;
+  if (document.cookie) {
+    for (i = 0; i <= cookie.length; i++) {
+      document.cookie = cookie[i].split("=")[0] + "=; expires=" + expiration;
+    }
+  }
+};
