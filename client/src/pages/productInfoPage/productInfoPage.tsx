18afaf848d32011b4f242f2a2b19a38391ce9176
NonFungibleCode <saechorome99@naver.com>
2023-07-18 14:01:27 +0900

    [design] 공지사항 컴포넌트 반응형 추가 및 css 수정

--- client/src/pages/productInfoPage/productInfoPage.tsx
+++ client/src/pages/productInfoPage/productInfoPage.tsx
@@ -8,23 +8,27 @@ export const ProductInfoPage = () => {
   return (
     <>
       <S.ProductInfo>
-        <S.ProductImage>상품이미지</S.ProductImage>
-        <S.ProductDetailContainer>
-          <S.ProductUpperPart>
-            <S.SalesButton>판매중</S.SalesButton>
-            <S.DeleteBtn>삭제</S.DeleteBtn>
-          </S.ProductUpperPart>
-          <S.ProductMiddlePart>
-            <S.CategoryButton>카테고리</S.CategoryButton>
-            <h2>맨투맨</h2>
-            <S.Condition src={ConditionImg} />
-          </S.ProductMiddlePart>
-          <S.ProductLowerPart>
-            <h2>30,000</h2>
-          </S.ProductLowerPart>
-        </S.ProductDetailContainer>
-        <S.ProductDescription>상품 설명</S.ProductDescription>
-        <S.ProductPurchase>구매하기</S.ProductPurchase>
+        <S.ContetntsWrapper>
+          <S.ProductImage>상품이미지</S.ProductImage>
+          <S.ProductDetailContainer>
+            <S.ProductUpperPart>
+              <S.SalesButton>판매중</S.SalesButton>
+              <S.DeleteBtn>삭제</S.DeleteBtn>
+            </S.ProductUpperPart>
+            <S.ProductMiddlePart>
+              <S.CategoryButton>카테고리</S.CategoryButton>
+              <h2>맨투맨</h2>
+              <S.Condition src={ConditionImg} />
+            </S.ProductMiddlePart>
+            <S.ProductLowerPart>
+              <h2>30,000</h2>
+            </S.ProductLowerPart>
+            <S.ProductDescription>상품 설명</S.ProductDescription>
+          </S.ProductDetailContainer>
+        </S.ContetntsWrapper>
+        <div>
+          <S.ProductPurchase>구매하기</S.ProductPurchase>
+        </div>
       </S.ProductInfo>
       <S.CommentContainer>코멘트</S.CommentContainer>
     </>
