6c3dc17525153c95444031e1a79494fde4df8321
오승찬 <oseungchan@oseungchan-ui-MacBookAir.local>
2023-07-20 19:55:08 +0900

    1차 승인관리

--- client/src/components/ApprovalTable/ApprovalTable.tsx
+++ client/src/components/ApprovalTable/ApprovalTable.tsx
@@ -1,19 +1,53 @@
-import { useEffect, useState } from "react";
-import * as S from "./styled";
-import { Collapse, Dialog, DialogActions, DialogContent } from "@mui/material";
 import axios from "axios";
-import { IMG_URL } from "@/constants/constants";
+import * as S from "./styled";
+import { ChangeEvent, useEffect, useState } from "react";
+import { BASE_URL, IMG_URL } from "@/constants/constants";
+import { Collapse, Dialog, DialogActions, DialogContent } from "@mui/material";
+import SelectBox from "../SelectBox/SelectBox";
+import { getToken } from "@/utils/token";
+import { insertComma } from "@/utils/inssertComma";
 
 interface ApprovalTableProps {
   product: {
     name: string;
-    category: string;
+    category: string | any;
     modifyAt: string;
-    imageLink: string
+    imageLink: string;
+    content: string;
+    memberId: number;
+    price: string;
+    productId: number;
   };
   idx: number;
   page: number;
 }
+const options = ["상의", "하의", "아우터", "신발", "기타"];
+const marks = [
+  {
+    value: 0,
+    label: "0",
+  },
+  {
+    value: 20,
+    label: "20",
+  },
+  {
+    value: 40,
+    label: "40",
+  },
+  {
+    value: 60,
+    label: "60",
+  },
+  {
+    value: 80,
+    label: "80",
+  },
+  {
+    value: 100,
+    label: "100",
+  },
+];
 const ApprovalTable: React.FC<ApprovalTableProps> = ({
   product,
   idx,
@@ -21,16 +55,54 @@ const ApprovalTable: React.FC<ApprovalTableProps> = ({
 }) => {
   const [listOpen, setListOpen] = useState<boolean>(false);
   const [dialogOpen, setDialogOpen] = useState(false);
+  const [productData, setProductData] = useState({
+    name: product.name,
+    title: "title",
+    content: product.content,
+    price: "0",
+    category: product.category,
+    imageLink: product.imageLink,
+    conditionValue: 0,
+    pointValue: "0",
+  });
   const handleClick = () => setListOpen(!listOpen);
   const handleClose = () => setDialogOpen(false);
-  const banclick = () => {
+  const handleProductData = (ev: ChangeEvent<HTMLInputElement>) => {
+    if (ev.target.name === "price") {
+      setProductData({ ...productData, price: insertComma(ev.target.value) });
+    } else if (ev.target.name === "pointValue") {
+      setProductData({
+        ...productData,
+        pointValue: insertComma(ev.target.value),
+      });
+    } else {
+      setProductData({ ...productData, [ev.target.name]: ev.target.value });
+    }
+  };
+  const approvalClick = () => {
     (async () => {
       try {
+        console.log(productData);
+        console.log(`${BASE_URL}/products/${product.productId}`);
+        console.log(getToken()[1]);
+        await axios.patch(
+          `${BASE_URL}/products/${product.productId}`,
+          productData,
+          {
+            headers: {
+              refresh: getToken()[1],
+            },
+          }
+        );
+        console.log("성공~");
       } catch (err) {
-        console.error("Error patching ban data", err);
+        console.error("Error uploading product.", err);
       }
     })();
   };
+  function valuetext(value: number) {
+    return `${value}`;
+  }
   useEffect(() => {
     setListOpen(false);
   }, [page]);
@@ -44,14 +116,83 @@ const ApprovalTable: React.FC<ApprovalTableProps> = ({
       </S.ProductsTable>
       <Collapse in={listOpen} timeout="auto" unmountOnExit>
         <S.Field>
+          <S.Content_1>
             <S.ProductImage src={`${IMG_URL}/${product.imageLink}`} />
+            <SelectBox
+              usage={"카테고리"}
+              options={options}
+              setOption={(value) => {
+                setProductData({ ...productData, category: value });
+              }}
+            />
+            <S.SliderContainer>
+              <S.InfoTitle>상품 컨디션</S.InfoTitle>
+              <S.CustomSlider
+                defaultValue={0}
+                getAriaValueText={valuetext}
+                step={null}
+                marks={marks}
+                onChange={(ev: any) => {
+                  setProductData({
+                    ...productData,
+                    conditionValue: ev.target.value / 10,
+                  });
+                }}
+              />
+            </S.SliderContainer>
+          </S.Content_1>
+          <S.Content_2>
+            <S.InfoContainer name={"name"}>
+              <S.InfoTitle>제목</S.InfoTitle>
+              <S.CustomTextField
+                name={"name"}
+                value={productData.name}
+                onChange={handleProductData}
+              />
+            </S.InfoContainer>
+            <S.InfoContainer name={"price"}>
+              <S.InfoTitle>가격</S.InfoTitle>
+              <S.PriceContainer>
+                <S.CustomTextField
+                  name={"price"}
+                  value={productData.price}
+                  onChange={handleProductData}
+                />
+                <S.Currency>원</S.Currency>
+              </S.PriceContainer>
+            </S.InfoContainer>
+            <S.InfoContainer name={"pointValue"}>
+              <S.InfoTitle>포인트</S.InfoTitle>
+              <S.CustomTextField
+                name={"pointValue"}
+                value={productData.pointValue}
+                onChange={handleProductData}
+              />
+            </S.InfoContainer>
+            <S.InfoContainer name={"content"}>
+              <S.InfoTitle>세부사항</S.InfoTitle>
+              <S.CustomTextField
+                name={"content"}
+                multiline
+                rows={10}
+                value={productData.content}
+                onChange={handleProductData}
+              />
+            </S.InfoContainer>
+            <S.BtnConatiner>
+              <S.ApprovalBtn approval={true} onClick={approvalClick}>
+                등록하기
+              </S.ApprovalBtn>
+              <S.ApprovalBtn approval={false}>반려하기</S.ApprovalBtn>
+            </S.BtnConatiner>
+          </S.Content_2>
         </S.Field>
       </Collapse>
       <Dialog open={dialogOpen} onClose={handleClose}>
         <DialogContent>해당 상품을 반송처리 하시겠습니까?</DialogContent>
         <DialogActions>
           <S.CancelBtn onClick={handleClose}>취소</S.CancelBtn>
-          <S.AgreeBtn onClick={banclick} autoFocus>
+          <S.AgreeBtn onClick={approvalClick} autoFocus>
             반송
           </S.AgreeBtn>
         </DialogActions>
