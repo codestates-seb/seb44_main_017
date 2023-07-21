ccd3ab2164ec3adddfe48ac3af44eb4270db641a
lhyjjg <leehyun147@gmail.com>
2023-07-21 12:15:10 +0900

    feat: 공지 상세페이지 구현 및 speedDial 컴포넌트 구현

--- client/src/components/Modal_login/LoginModal.tsx
+++ client/src/components/Modal_login/LoginModal.tsx
@@ -101,7 +101,7 @@ const LoginModal = ({ closeModal }: Props) => {
   };
   return (
     <S.Container onClick={closeModal}>
-      <S.Content onClick={e => e.stopPropagation()}>
+      <S.Content onClick={(e) => e.stopPropagation()}>
         <S.CloseButton onClick={closeModal} />
         <S.LoginTitleContainer>
           <Logo width="40px" height="24px" />
@@ -115,7 +115,7 @@ const LoginModal = ({ closeModal }: Props) => {
           type="text"
           value={userName}
           placeholder="이메일을 입력해주세요."
-          onChange={e => setUserName(e.target.value)}
+          onChange={(e) => setUserName(e.target.value)}
           style={{ marginBottom: "48px" }}
         />
         <S.PasswordLabel>
@@ -123,7 +123,7 @@ const LoginModal = ({ closeModal }: Props) => {
             type={showPassword ? "text" : "password"}
             value={password}
             placeholder="비밀번호를 입력해주세요."
-            onChange={e => setPassword(e.target.value)}
+            onChange={(e) => setPassword(e.target.value)}
           />
           <S.VisibilityButton onClick={passwordVisibility} />
         </S.PasswordLabel>
@@ -139,7 +139,7 @@ const LoginModal = ({ closeModal }: Props) => {
           <input
             type="checkbox"
             checked={isAdmin}
-            onChange={e => setIsAdmin(e.target.checked)}
+            onChange={(e) => setIsAdmin(e.target.checked)}
             style={{ cursor: "pointer", marginRight: "8px" }}
           />
           관리자로 로그인하기
