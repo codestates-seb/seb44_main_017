cb5ebb90e8da517dbc21c444c7b66f4ed6273588
hwanghojun <mjhhj1362@gmail.com>
2023-07-21 17:25:15 +0900

    [feat] notify 다음글/이전글 구현

--- server/src/main/java/com/main/project/notifyBoard/repository/NotifyRepository.java
+++ server/src/main/java/com/main/project/notifyBoard/repository/NotifyRepository.java
@@ -3,6 +3,15 @@ package com.main.project.notifyBoard.repository;
 import com.main.project.notifyBoard.entity.NotifyBoard;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface NotifyRepository extends JpaRepository<NotifyBoard,Long> {
+
+    @Query(value = "select Min(board_id) from notify_board b where b.board_id > :boardId",nativeQuery = true)
+    Optional<Long>findByBoardIdGreaterThan(Long boardId);
+
+    @Query(value = "select Max(board_id) from notify_board b where b.board_id < :boardId",nativeQuery = true)
+    Optional<Long>findByBoardIdLessThan(Long boardId);
 }
