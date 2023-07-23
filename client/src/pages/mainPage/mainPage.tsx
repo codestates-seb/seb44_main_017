f5a20951f98d52d5bb826fb41daaec6711e3ba52
오승찬 <oseungchan@oseungchan-ui-MacBookAir.local>
2023-07-23 19:12:29 +0900

    1차 수정

--- client/src/pages/mainPage/mainPage.tsx
+++ client/src/pages/mainPage/mainPage.tsx
@@ -1,22 +1,58 @@
 import FlowDescription from "@/pages/mainPage/FlowDescription/FlowDescription";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import BannerContainerPc from "./Banners/bannerContainerPc";
 import BannerContainerMobile from "./Banners/bannerContainerMobile";
 import AffiliatedCompanies from "./AffiliatedCompanies/AffiliatedCompanies";
 import ClothesList from "./ClothesList/ClothesList";
+import { useEffect, useState } from "react";
+import { Alert } from "@mui/material";
+import { useNavigate } from "react-router";
+import { userInfoState } from "@/recoil/atom";
+import { useRecoilValue } from "recoil";
+import { getToken } from "@/utils/token";
 
 export const MainPage = () => {
+  const navigate = useNavigate();
+  const userInfo = useRecoilValue(userInfoState);
+  const [open, setOpen] = useState(false);
+  const [error, setError] = useState<string>("");
+  const clickCollection = () => {
+    if (!getToken()[1]) {
+      setError("로그인이 필요합니다.");
+      setOpen(true);
+    } else if (userInfo?.role === "admin") {
+      setError("관리자 계정으로는 수거신청이 불가능합니다.");
+      setOpen(true);
+    } else {
+      navigate("/collection");
+    }
+  };
+  useEffect(() => {
+    if (open) {
+      const timer = setTimeout(() => {
+        setOpen(false);
+      }, 4000);
+      return () => clearTimeout(timer);
+    }
+  }, [open]);
   return (
     <MainPageContainer>
       <MainContentContainer>
         <BannerContainer>
-          <BannerContainerPc />
-          <BannerContainerMobile />
+          <BannerContainerPc clickCollection={clickCollection} />
+          <BannerContainerMobile clickCollection={clickCollection} />
         </BannerContainer>
         <ClothesList />
       </MainContentContainer>
       <FlowDescription />
       <AffiliatedCompanies />
+      {open ? (
+        <CustomAlert variant="filled" severity="error">
+          {error}
+        </CustomAlert>
+      ) : (
+        <></>
+      )}
     </MainPageContainer>
   );
 };
@@ -34,10 +70,28 @@ const MainContentContainer = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
-  min-height: calc(100vh - 72px);
+  @media (min-width: 768px) {
+    justify-content: space-around;
+    min-height: calc(100vh - 72px);
+  }
 `;
 const BannerContainer = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
+`;
+const slideFromTop = keyframes`
+  from {
+    transform: translateY(-100%);
+  }
+  to {
+    transform: translateY(0);
+  }
+`;
+const CustomAlert = styled(Alert)`
+  position: absolute;
+  top: 80px;
+  border-radius: 99px !important;
+  animation: ${slideFromTop} 1s ease;
+  z-index: 99;
 `;
