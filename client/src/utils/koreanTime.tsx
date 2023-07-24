d10dc02f93e1af0a9434e9a0a8160d5558db0e15
오승찬 <oseungchan@oseungchan-ui-MacBookAir.local>
2023-07-24 21:42:33 +0900

    fix

--- client/src/utils/koreanTime.tsx
+++ client/src/utils/koreanTime.tsx
@@ -23,5 +23,6 @@ export const koreanTime = (time: any) => {
   const hours = String(date.getHours() - 3).padStart(2, "0");
   const minutes = String(date.getMinutes()).padStart(2, "0");
   const seconds = String(date.getSeconds()).padStart(2, "0");
+  
   return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 };
