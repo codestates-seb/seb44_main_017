961cd7d3bf5b80ea201e95053e19e56f31bdcbb9
오승찬 <oseungchan@oseungchan-ui-MacBookAir.local>
2023-07-24 20:13:37 +0900

    fix

--- client/src/utils/inssertComma.tsx
+++ client/src/utils/inssertComma.tsx
@@ -1,21 +1,28 @@
 export const insertComma = (value: string): string => {
-    if (!/^[0-9,]*$/.test(value)) {
-      throw new Error("Input must contain only numbers and commas");
+  if (!/^[0-9,]*$/.test(value)) {
+    throw new Error("Input must contain only numbers and commas");
+  }
+
+  const cleanInput = value.replace(/,/g, "");
+  let result = "";
+  let digitCount = 0;
+
+  for (let i = cleanInput.length - 1; i >= 0; i--) {
+    result = cleanInput[i] + result;
+    digitCount++;
+
+    if (digitCount === 3 && i !== 0) {
+      result = "," + result;
+      digitCount = 0;
     }
-  
-    const cleanInput = value.replace(/,/g, "");
-    let result = "";
-    let digitCount = 0;
-  
-    for (let i = cleanInput.length - 1; i >= 0; i--) {
-      result = cleanInput[i] + result;
-      digitCount++;
-  
-      if (digitCount === 3 && i !== 0) {
-        result = "," + result;
-        digitCount = 0;
-      }
-    }
-  
-    return result;
-  };
+  }
+
+  return result;
+};
+export const commaNumber = (num: number | string): string => {
+  const numString = num.toString();
+  const numParts = numString.split(".");
+  numParts[0] = numParts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+  return numParts.join(".");
+};
