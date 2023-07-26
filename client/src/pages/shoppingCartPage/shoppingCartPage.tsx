6ab577f4df9820b23ddbf2f208399f8e19b34169
pparkjyy <jyp96117@naver.com>
2023-07-27 02:47:44 +0900

    # fix : 결제시 포인트부족, 주소없음, 물품선택 안했을시 알람

--- client/src/pages/shoppingCartPage/shoppingCartPage.tsx
+++ client/src/pages/shoppingCartPage/shoppingCartPage.tsx
@@ -53,6 +53,10 @@ const ShoppingCartPage = () => {
   };
 
   const orderItems = async () => {
+    if(postCode.address == ""){
+      alert("주소를 입력해 주세요.")
+    }else{
+    try{
     const { data, status } = await axios.post(
       BASE_URL + "/kakaoPaybucket",
       {
@@ -75,7 +79,18 @@ const ShoppingCartPage = () => {
       window.open(data);
       navigate("/productlist");
     }
+  } catch (error: any) {
+    if (error.response.status === 409) {
+      console.log(error.response);
+      if(error.response.data.message == "No products in order"){
+        alert("결제할 물품을 선택해 주세요.")
+      }else if(error.response.data.message == "point_is_not_enough"){
+        alert("포인트가 부족합니다.");
+      }
+    }
+  }
     paymentList = ",";
+  }
   };
 
   const deleteItem = (id: number) => {
