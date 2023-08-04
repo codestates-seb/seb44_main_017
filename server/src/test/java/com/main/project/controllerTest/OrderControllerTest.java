11cf1bf81b634a3559d0e65a13879a6492f6ba62
pparkjyy <jyp96117@naver.com>
2023-08-04 17:09:55 +0900

    Create OrderControllerTest.java

---
+++ server/src/test/java/com/main/project/controllerTest/OrderControllerTest.java
@@ -0,0 +1,9 @@
+package com.main.project.controllerTest;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class OrderControllerTest {
+}
